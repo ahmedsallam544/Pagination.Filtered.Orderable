d492b38ae73cf92125e6721c00e01b0e84bb799d
Adel <ahmed.sallam@intalio.com>
2024-05-11 22:18:15 +0300

    Add project files.

---
+++ Pagedtems.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagination
+{
+    public class Pagedtems<T>
+    {
+        protected long PageIndex { get; }
+
+        public long PageSize { get; }
+
+        public long PagesCount { get; }
+
+        public long ItemsCount { get; }
+
+        public IReadOnlyCollection<T> Items { get; }
+        public Pagedtems(IQueryable<T> query, int pageIndex, int pageSize, long? totalItemsCount)
+        {
+            ArgumentNullException.ThrowIfNull(query);
+            long itemsCount = totalItemsCount is null ? query.CountBack(pageIndex,pageSize) : totalItemsCount.GetValueOrDefault();
+            
+            Items = query.Paginate(pageIndex, pageSize).ToImmutableArray();
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            ItemsCount = itemsCount;
+            PagesCount = pageSize != 0L ? (long)Math.Ceiling(ItemsCount / (Decimal)pageSize) : 0L;
+        }
+        public Pagedtems(IQueryable<T> query, int pageIndex, int pageSize, long? totalItemsCount, Expression<Func<T, bool>> filter = null,
+         Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+         Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null)
+        {
+            ArgumentNullException.ThrowIfNull(query);
+            long itemsCount = totalItemsCount is null ? query.CountBack(pageIndex, pageSize) : totalItemsCount.GetValueOrDefault();
+
+            Items = query.Paginate(pageIndex, pageSize, filter, include, orderBy).ToImmutableArray();
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            ItemsCount = itemsCount;
+            PagesCount = pageSize != 0L ? (long)Math.Ceiling(ItemsCount / (Decimal)pageSize) : 0L;
+        }
+        public async Task<long> CountItemsAsync(IQueryable<T> query )
+        {
+            ArgumentNullException.ThrowIfNull(query);
+
+            int result = await query.CountAsync();
+
+            return result;
+        }
+    }
+}
---
+++ QueryableExtensions.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Pagination
+{
+    public static class QueryableExtensions
+    {
+        const int DefaultPage = 1;
+        const int DefaultPageSize = 10;
+
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> query, int? page = DefaultPage, int? pageSize = DefaultPageSize)
+        {
+            if (query is null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
+
+            return query.Skip((page.Value - 1) * pageSize.Value).Take(pageSize.Value);
+        }
+
+         public static IQueryable<T> Paginate<T>(
+         this IQueryable<T> query,
+         int page = DefaultPage,
+         int pageSize = DefaultPageSize,
+         Expression<Func<T, bool>> filter = null,
+         Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
+         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+        {
+            if (query == null)
+                throw new ArgumentNullException(nameof(query));
+
+            if (page <= 0 || pageSize <= 0)
+                throw new ArgumentException("Page and page size must be positive integers.");
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (include != null)
+                query = include(query);
+
+            if (orderBy != null)
+                query = orderBy(query);
+              
+
+            return query.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+    
+        public static long CountBack<T>(
+         this IQueryable<T> query,
+         int page = DefaultPage,
+         int pageSize = DefaultPageSize,
+         Expression<Func<T, bool>> filter = null,
+         Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
+         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+        {
+            if (query is null)
+                throw new ArgumentNullException(nameof(query));
+
+            if (page <= 0 || pageSize <= 0)
+                throw new ArgumentException("Page and page size must be positive integers.");
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (include != null)
+                query = include(query);
+
+            if (orderBy != null)
+                query = orderBy(query);
+
+            return query.Count<T>();
+        }
+        public static IQueryable<T> FirstPage<T>(this IQueryable<T> query, int pageSize)
+            => query.Take(pageSize);
+
+        public static IQueryable<T> FirstPage<T>(this IQueryable<T> query, int pageSize,
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+        {
+            if (query is null)
+                throw new ArgumentNullException(nameof(query));
+
+            if ( pageSize <= 0)
+                throw new ArgumentException("page size must be positive integers.");
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (include != null)
+                query = include(query);
+
+            if (orderBy != null)
+                query = orderBy(query);
+            
+         
+          return  query.Take(pageSize);
+        }
+
+        public static IQueryable<T> LastPage<T>(this IQueryable<T> query, int pageSize)
+            => query.TakeLast(pageSize);
+
+        public static IQueryable<T> LastPage<T>(this IQueryable<T> query, int pageSize ,
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+        {
+            if (query is null)
+                throw new ArgumentNullException(nameof(query));
+
+            if (pageSize <= 0)
+                throw new ArgumentException("page size must be positive integers.");
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (include != null)
+                query = include(query);
+
+            if (orderBy != null)
+                query = orderBy(query);
+            
+         
+            return query.TakeLast(pageSize);
+        }
+        public static int CountOfPages<T>(this IQueryable<T> query, int pageSize)
+        {
+            var total = query.Count();
+            return (total / pageSize) + ((total % pageSize) > 0 ? 1 : 0);
+        }
+
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            return condition
+                ? query.Where(predicate)
+                : query;
+        }
+
+    }
+}
